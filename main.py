4b02b189a2cc50081337fcab3afc5bb6d87794ea
Aminu Faezol <32818731+FaezolCode@users.noreply.github.com>
2021-07-06 16:25:01 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+import random
+
+#flipping_coin = input("type toss to play now")
+random_int = random.randint(1, 6)
+if random_int == 6:
+  print("you win")
+elif random_int == 1:
+  print ("you lost")
+elif random_int == 2:
+  print ("you lost")
+elif random_int == 3:
+  print ("you lost")
+elif random_int == 4:
+  print ("you lost")
+elif random_int == 5:
+  print ("you lost")
+else:
+  print("you have chosen a wrong number")
+
+
+
+
+
+
+
+
+
+
+
